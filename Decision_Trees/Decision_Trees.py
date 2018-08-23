6bdceb0e39142a028cd2be9a28ec09ceb7825d94
junaidnz97 <junaidnz97@gmail.com>
2018-08-24 01:10:33 +0530

    Added predict function for decision trees

--- Decision_Trees/Decision_Trees.py
+++ Decision_Trees/Decision_Trees.py
@@ -1,5 +1,6 @@
 from math import log
 import numpy as np
+from collections import Counter
 
 class Node:
 	def __init__(self):
@@ -7,7 +8,8 @@ class Node:
 		self.split_value=None
 		self.left=None
 		self.right=None
-		
+		self.node_def=True
+		self.label=None
 
 class decision_tree:
 
@@ -21,8 +23,11 @@ class decision_tree:
 		self.head=self.build(self.head,x_train,y_train,self.root_entropy,1) 
 	
 	def build(self,current_node,x_train,y_train,entropy_parent,cur_depth):
-		if(cur_depth>self.max_depth or len(x_train)<=self.min_samples ):
-			return None	
+		if(cur_depth>self.max_depth or len(y_train)<=self.min_samples ):
+			temp_node=Node()
+			temp_node.node_def=False
+			return temp_node	
+
 		row_length=len(x_train)
 		col_length=len(x_train[0])
 		count={}
@@ -31,6 +36,7 @@ class decision_tree:
 		information_gain={}
 		weighted_avg={}
 		output_set=set(y_train)
+		
 		for i in range(0,col_length):
 			count[i]={}
 			prob[i]={}
@@ -42,10 +48,12 @@ class decision_tree:
 				for k in output_set:
 					count[i][j+"and"+k]=0
 					prob[i][j+"and"+k]=0
+		
 		for i in range(0,col_length):
 			for j in range(0,row_length):
 				count[i][x_train[j][i]]=count[i][x_train[j][i]]+1
 				count[i][x_train[j][i]+"and"+y_train[j]]=count[i][x_train[j][i]+"and"+y_train[j]]+1
+		
 		for i in range(0,col_length):
 			s=set(x_train[:,i])
 			weighted_avg[i]=0
@@ -62,32 +70,22 @@ class decision_tree:
 				temp_sum=temp_sum+count[i][j]
 			weighted_avg[i]=weighted_avg[i]/temp_sum
 			information_gain[i]=entropy_parent-weighted_avg[i]
+		
 		max_key=max(information_gain,key=information_gain.get)
 		split_set=set(x_train[:,max_key])
 		split_value=None
+		
 		for i in split_set:
 			split_value=i
 			break
-		print(information_gain)
+		
 		x_train_left=[]
 		x_train_right=[]
 		y_train_left=[]
 		y_train_right=[]
-		#print(information_gain)
-		'''for k,i in enumerate(split_set):
-			split_data=[]
-			for j in range(0,row_length):
-				if(x_train[j,max_key]==i):
-					split_data.append(j)
-			if(k==0):
-				x_train_left=x_train[split_data]
-				y_train_left=y_train[split_data]
-			else:
-				x_train_right=x_train[split_data]
-				y_train_right=y_train[split_data]'''
-
 		split_data_left=[]
 		split_data_right=[]
+		
 		for j in range(0,row_length):
 			if(x_train[j,max_key]==split_value):
 				split_data_left.append(j)
@@ -100,11 +98,38 @@ class decision_tree:
 		y_train_right=y_train[split_data_right]
 		current_node.split_column=max_key
 		current_node.split_value=split_value
+		temp_dict={}
+		
+		for i in y_train:
+			temp_dict[i]=0
+		for i in y_train:
+			temp_dict[i]=temp_dict[i]+1
+		
+		current_node.label=Counter(temp_dict).most_common(1)[0][0]
 		current_node.left=Node()
 		current_node.right=Node()
-		current_node.left=self.build(current_node.left,x_train_left,y_train_left,information_gain[max_key],cur_depth+1)
-		current_node.right=self.build(current_node.right,x_train_right,y_train_right,information_gain[max_key],cur_depth+1)
+		current_node.left=self.build(current_node.left,x_train_left,y_train_left,entropy[current_node.split_column][current_node.split_value],cur_depth+1)
+		current_node.right=self.build(current_node.right,x_train_right,y_train_right,entropy[current_node.split_column][current_node.split_value],cur_depth+1)
+				
 		return current_node
+
+
+	def predict(self,test):
+		temp_list=[]
+		for i in test:
+			temp_list.append(self.test_fun(self.head,i))
+		return temp_list
+
+	def test_fun(self,cur_node,test):
+
+		if(cur_node.left.node_def is False and cur_node.right.node_def is False):
+			return cur_node.label
+		
+		if(test[cur_node.split_column]==cur_node.split_value):
+			return self.test_fun(cur_node.left,test)
+		else:
+			return self.test_fun(cur_node.right,test)
+		
 
 
 x_train=[["Steep","Bumpy","Yes"],["Steep","Smooth","Yes"],["Flat","Bumpy","No"],["Steep","Smooth","No"]]
@@ -113,6 +138,6 @@ y_train=["Slow","Slow","Fast","Fast"]
 x_train=np.array(x_train)
 y_train=np.array(y_train)
 
-
 clf=decision_tree()
 clf.train(x_train,y_train)
+print(clf.predict(x_train))
